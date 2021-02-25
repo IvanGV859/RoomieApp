b74b4d03f6536d72a8014ebce552167d1be49b62
OmarLopez1016 <79167831+omarlopez1016@users.noreply.github.com>
2021-02-25 16:02:34 -0600

    Nuevo intento

--- app/src/main/java/com/example/roomieapp/MainActivity.java
+++ app/src/main/java/com/example/roomieapp/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //fklnsflknflkbv
                 //Esto es un comentario
+        //Este es otro comentario
     }
 }
