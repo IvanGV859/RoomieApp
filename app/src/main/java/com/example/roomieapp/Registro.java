6b5e88d944654fb897f86e13938918701c8ba57d
OmarLopez1016 <79167831+omarlopez1016@users.noreply.github.com>
2021-02-27 16:48:57 -0600

    Pantalla registro y conexion a Firebase

--- app/src/main/java/com/example/roomieapp/Registro.java
+++ app/src/main/java/com/example/roomieapp/Registro.java
@@ -6,20 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //fklnsflknflkbv
-        //Esto es un comentario
-        //Este es otro comentario
-        //Nuevo nuevo comentari
+        setContentView(R.layout.activity_registro);
     }
 
-    public void irRegistro (View view) {
+    public  void Reinicio (View view) {
         Intent i = new Intent(this, Registro.class);
         startActivity(i);
+        finish();
     }
 }
