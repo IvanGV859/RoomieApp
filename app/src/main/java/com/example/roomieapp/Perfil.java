b639d6654fb36e090de47e276e4f3f74603ac732
OmarLopez1016 <79167831+omarlopez1016@users.noreply.github.com>
2021-05-12 22:30:17 -0500

    Perfil de usuario

--- app/src/main/java/com/example/roomieapp/Perfil.java
+++ app/src/main/java/com/example/roomieapp/Perfil.java
@@ -1,15 +1,43 @@
 package com.example.roomieapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.roomieapp.adapter.AdapterDepartamentos;
+import com.example.roomieapp.adapter.AdapterDepartamentosPerfil;
+import com.example.roomieapp.pojo.Departamentos;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.EventListener;
 
 public class Perfil extends AppCompatActivity {
 
     //Initialize variable
     DrawerLayout drawerLayout;
+
+    DatabaseReference databaseReference;
+    ArrayList<Departamentos> list;
+    RecyclerView recyclerView;
+    AdapterDepartamentosPerfil adapter;
+
+    private LinearLayoutManager lm;
+    private FirebaseAuth mAuth;
+    private TextView nombreUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +46,53 @@ public class Perfil extends AppCompatActivity {
 
         //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        mAuth = FirebaseAuth.getInstance();
+        String id = mAuth.getCurrentUser().getUid();
+        nombreUsuario = (TextView) findViewById(R.id.txt_nombreUsuario);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        recyclerView = findViewById(R.id.recyclerView_Perfil);
+        lm = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(lm);
+        list = new ArrayList<>();
+        adapter = new AdapterDepartamentosPerfil(list);
+        recyclerView.setAdapter(adapter);
+
+        databaseReference.child("Departamentos").orderByChild("Usuario").equalTo(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Departamentos ms = dataSnapshot.getValue(Departamentos.class);
+                        list.add(ms);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        databaseReference.child("Usuarios").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String nombre = snapshot.child("name").getValue().toString();
+                nombreUsuario.setText(nombre);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 
     public void ClickMenu(View view){
         //Open drawer
